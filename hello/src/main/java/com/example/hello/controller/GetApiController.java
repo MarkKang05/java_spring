23752f66c8d4e41acc234539d622e2f0524d8723
MarkKang05 <markkang05@gmail.com>
2021-08-13 21:55:51 +0900

    first commit

---
+++ hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -0,0 +1,67 @@
+package com.example.hello.controller;
+
+import com.example.hello.dto.UserRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/get")
+public class GetApiController {
+    @GetMapping(path = "/hello")
+    public String hello(){
+        return "get hello";
+    }
+
+    @RequestMapping(path = "/hi", method = RequestMethod.GET)
+    public String hi(){
+        return "hi";
+    }
+
+    @GetMapping("/path-variable/{name}")
+    public String pathVariable(@PathVariable(name = "name") String pathName, String name) {
+        System.out.println("PathVariable name : "+pathName);
+        return pathName;
+    }
+
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach( entry -> {
+
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey()+" + "+entry.getValue()+"\n");
+        });
+        return sb.toString();
+    }
+
+    @GetMapping("query-param02")
+    public String queryParma02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+        return name+" "+ email +" " + age;
+
+    }
+
+
+    @GetMapping("query-param03")
+    public String queryParma03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+        return userRequest.toString();
+
+    }
+
+
+}
