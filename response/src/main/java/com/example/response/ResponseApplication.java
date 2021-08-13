e80a098adab1992d723856a6ac04e6d30ec6ae78
MarkKang05 <markkang05@gmail.com>
2021-08-13 23:13:45 +0900

    Add put

--- response/src/main/java/com/example/response/ResponseApplication.java
+++ response/src/main/java/com/example/response/ResponseApplication.java
@@ -1,14 +1,13 @@
-package com.example.delete;
+package com.example.response;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeleteApplication {
+public class ResponseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeleteApplication.class, args);
+        SpringApplication.run(ResponseApplication.class, args);
     }
-
 
 }
