3090742f5b6e5c0a4ffe37f959239484b1ea669d
MarkKang05 <markkang05@gmail.com>
2021-08-17 22:44:04 +0900

    Restaurant Memory CRUD DB

--- restaurant/src/main/java/com/example/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantApplication {
+    public class RestaurantApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantApplication.class, args);
+    }
 
 }
+
--- restaurant/src/main/java/com/example/restaurant/db/MemoryDbRepositoryIfs.java
+++ restaurant/src/main/java/com/example/restaurant/db/MemoryDbRepositoryIfs.java
@@ -1,9 +1,11 @@
 package com.example.restaurant.db;
 
+import java.util.List;
 import java.util.Optional;
 
-public class MemoryDbRepositoryIfs<T> {
-
+public interface MemoryDbRepositoryIfs<T> {
     Optional<T> findById(int index);
-
+    T save(T entity);
+    void deleteById(int index);
+    List<T> listAll();
 }
