d51029d14b65db9ce9213cfb19ced7fe02d3d4c8
MarkKang05 <markkang05@gmail.com>
2021-08-20 23:49:17 +0900

    todo-list: controller, model, repository, service

---
+++ todo-sercer/src/main/java/org/example/TodoServerApplication.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class TodoServerApplication {
+    public static void main(String[] args) {
+        //System.out.println("hello world");
+        SpringApplication.run(TodoServerApplication.class, args);
+    }
+}
