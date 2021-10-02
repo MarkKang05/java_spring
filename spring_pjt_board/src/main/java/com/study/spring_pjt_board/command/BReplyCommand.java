d15f5e8036fdacb7f8a06fa58e5087b1146d23e4
macbook13 <markkang05@macbook13ui-MacBookAir.local>
2021-10-02 16:52:24 +0900

    spring_mvc_board: non-JDBC.ver

--- spring_pjt_board/src/main/java/com/study/spring_pjt_board/command/BReplyCommand.java
+++ spring_pjt_board/src/main/java/com/study/spring_pjt_board/command/BReplyCommand.java
@@ -1,11 +1,29 @@
 package com.study.spring_pjt_board.command;
 
+import com.study.spring_pjt_board.dao.BDao;
 import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 public class BReplyCommand implements BCommand{
 
     @Override
     public void execute(Model model) {
+
+        Map<String, Object> map = model.asMap();
+        HttpServletRequest request = (HttpServletRequest) map.get("request");
+
+        String bId = request.getParameter("bId");
+        String bName = request.getParameter("bName");
+        String bTitle = request.getParameter("bTitle");
+        String bContent = request.getParameter("bContent");
+        String bGroup = request.getParameter("bGroup");
+        String bStep = request.getParameter("bStep");
+        String bIndent = request.getParameter("bIndent");
+
+        BDao dao = new BDao();
+        dao.reply(bId, bName, bTitle, bContent, bGroup, bStep, bIndent);
 
     }
 
--- spring_pjt_board/src/main/java/com/study/spring_pjt_board/command/BReplyViewCommand.java
+++ spring_pjt_board/src/main/java/com/study/spring_pjt_board/command/BReplyViewCommand.java
@@ -1,12 +1,25 @@
 package com.study.spring_pjt_board.command;
 
+import com.study.spring_pjt_board.dao.BDao;
+import com.study.spring_pjt_board.dto.BDto;
 import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 public class BReplyViewCommand implements BCommand{
 
     @Override
     public void execute(Model model) {
+        Map<String, Object> map = model.asMap();
+        HttpServletRequest request = (HttpServletRequest) map.get("request");
+        String bId = request.getParameter("bId");
+        System.out.println(bId);
 
+        BDao dao = new BDao();
+        BDto dto = dao.reply_view(bId);
+
+        model.addAttribute("reply_view", dto);
     }
 
 }
